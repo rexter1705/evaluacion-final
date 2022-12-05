e8e018d3e8ab8132b179eb34a670010739e8b552
rexter1705 <119522743+rexter1705@users.noreply.github.com>
2022-12-05 12:13:24 -0600

    Add files via upload

---
+++ FIXWEIGHT/main.py
@@ -0,0 +1,13 @@
+
+for _ in range(int(input())):
+    n, w = map(int, input().split())
+    cnt = 0
+    for i in range(1, n + 1):
+        if w % i == 0:
+            a = w // i
+            if a <= n and a <= (abs(i - n) + 1):
+                cnt += 1
+    if cnt == 0:
+        print("NO")
+    else:
+        print("YES")
